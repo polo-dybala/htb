b550ff11fa59af8e5d26c4683a76d2889c9bf759
polo-dybala <nongweabraham5@gmail.com>
2026-08-21 02:00:51 +0200

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ export default defineConfig({
   // Update these two values to match your GitHub repo before deploying.
   // - If your repo is named "tonuser.github.io", remove the `base` line entirely.
   // - Otherwise, base must match your repo name exactly, with a leading slash.
-  site: 'https://tonuser.github.io',
-  base: '/htb-writeups',
+  site: 'https://polo-dybala.github.io',
+  base: '/writeups',
 
   markdown: {
     shikiConfig: {
